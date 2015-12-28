8f6f369385ef7537c853fb15de32bf447a33ec98
Terence Siganakis <terence@siganakis.com>
2015-12-29 09:02:32 +1100

    Renaming stuff so that it actually works

--- src/GrowingData.Mung.Web/compiler/preprocess/RazorPreCompilation.cs
+++ src/GrowingData.Mung.Web/compiler/preprocess/RazorPreCompilation.cs
@@ -2,8 +2,7 @@
 using Microsoft.AspNet.Mvc.Razor.Precompilation;
 using Microsoft.Extensions.PlatformAbstractions;
 
-namespace MusicStore
-{
+namespace GrowingData.Mung.Web {
     public class RazorPreCompilation : RazorPreCompileModule
     {
         public RazorPreCompilation(IApplicationEnvironment applicationEnvironment)
