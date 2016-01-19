b603c5c6b97dbf218576215efa49a6b2b0ecf8e1
Terence Siganakis <terence@siganakis.com>
2016-01-20 07:26:44 +1100

    No pausing on startup for debugger

--- src/GrowingData.Mung.Web/App/MungApp.cs
+++ src/GrowingData.Mung.Web/App/MungApp.cs
@@ -75,8 +75,8 @@ namespace GrowingData.Mung.Web {
 		}
 
 		public MungApp(IHostingEnvironment env) {
-			Console.WriteLine("Paused Initializing of MungApp, press a key to continue...");
-			Console.ReadKey();
+			//Console.WriteLine("Paused Initializing of MungApp, press a key to continue...");
+			//Console.ReadKey();
 
 
 			_pipeline = new EventPipeline();
