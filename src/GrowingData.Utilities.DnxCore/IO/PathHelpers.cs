0d587fe616e27b2895a0ae77ead69d8152c31a4c
Terence Siganakis <terence@siganakis.com>
2016-01-20 08:52:41 +1100

    Directory handling

---
+++ src/GrowingData.Utilities.DnxCore/IO/PathHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace GrowingData.Utilities.DnxCore {
+	public class PathHelpers {
+		public static void CreateDirectoryRecursively(string path) {
+			string[] pathParts = path.Split('\\');
+
+			for (int i = 0; i < pathParts.Length; i++) {
+				if (i > 0) {
+					pathParts[i] = Path.Combine(pathParts[i - 1], pathParts[i]);
+				}
+				if (!Directory.Exists(pathParts[i])) {
+					Directory.CreateDirectory(pathParts[i]);
+				}
+			}
+		}
+	}
+}
+
