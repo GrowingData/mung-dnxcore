870423fe1a199abb8e5a0a92913001fbc1bfcf04
Terence Siganakis <terence@siganakis.com>
2016-04-13 11:55:16 +1000

    AuthController for getting access tokens for logged in users

---
+++ src/GrowingData.Mung.Web/Areas/ExternalApi/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using GrowingData.Utilities.DnxCore;
+using GrowingData.Mung.Core;
+using GrowingData.Mung.Web.Models;
+using System.IO;
+using System.Collections.Generic;
+using JWT;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GrowingData.Mung.Web.Areas.ExternalApi.Controllers {
+
+	public class AuthController : ExternalApiController {
+
+
+		[Route("ext/v1/auth/access_token")]
+		[HttpGet]
+		public ActionResult AccessToken() {
+
+			var identity = HttpContext.CurrentMungerIdentity();
+			if (identity != null) {
+				var app = App.MungInternal;
+
+				var payload = new Dictionary<string, object>() {
+					{"clientTime", DateTime.UtcNow },
+					{"name", identity.User.Name },
+					{"email", identity.User.Email }
+				};
+				string token = JWT.JsonWebToken.Encode(payload, app.AppSecret, JwtHashAlgorithm.HS256);
+
+				return new ApiResult(new { accessToken = token, success = true });
+
+			}
+			return new ApiResult(new { success = false, message = "Not logged in" });
+		}
+	}
+}
